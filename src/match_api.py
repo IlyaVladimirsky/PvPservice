7704c1ebe5dcaf2fd1c554b4296b786da3f14d64
Ilya Vladimirsky <ivv.user@gmail.com>
2016-11-18 16:50:11 +0300

    [~]{move} loggers inside classes

--- src/match_api.py
+++ src/match_api.py
@@ -1,18 +1,32 @@
 import asyncio
-
+import logging
 from flask import request, jsonify
 from flask_restful import Resource
 
+import src.logger as logger
+
 
 class MatchResource(Resource):
-    def __init__(self, logic, loop):
+    """Class docs here"""
+
+    def __init__(self, logic, loop, debug=True):
+        """Init docs here"""
         self.logic = logic
         self.loop = loop
+
+        self.logger = logger.get_logger(
+            name=__name__,
+            path='logs/debug.log',
+            level=logging.DEBUG,
+            formatter='%(asctime)s - %(name)s - %(levelname)s - %(message)s'
+        )
+        self._debug = debug
 
     def get(self):
         pass
 
     def post(self):
+        """Method docs here"""
         data = request.json
         if 'nickname' not in data:
             return jsonify(self.logic.get_error_response('Incorrect POST parameters'))
@@ -22,6 +36,7 @@ class MatchResource(Resource):
                 'ip':       request.environ['REMOTE_ADDR']
             }
 
+            self.logger.debug('[post]: %s before event loop', client_info['nickname'])
             asyncio.set_event_loop(self.loop)
             task = asyncio.ensure_future(self.logic.create_match_request(**client_info))
             self.loop.run_until_complete(task)
--- src/match_logic.py
+++ src/match_logic.py
@@ -1,26 +1,16 @@
 import asyncio
 import logging
-# from threading import Event, Lock
 from asyncio import Event, Lock
 
 import src.logger as logger
 from .singleton import Singleton
 
 
-logger = logger.get_logger(
-    name=__name__,
-    path='logs/debug.log',
-    level=logging.DEBUG,
-    formatter='%(asctime)s - %(name)s - %(levelname)s - %(message)s'
-)
-logger.debug('{:*^50}'.format('New start'))
-
-
 @Singleton
 class MatchLogic:
     """Class docs here"""
 
-    def __init__(self, clients_number=2, match_wait_time=1):
+    def __init__(self, clients_number=2, match_wait_time=1, debug=True):
         """Init docs here"""
 
         # TODO: db class
@@ -33,10 +23,18 @@ class MatchLogic:
 
         self._match_id = None
 
+        self.logger = logger.get_logger(
+            name=__name__,
+            path='logs/debug.log',
+            level=logging.DEBUG,
+            formatter='%(asctime)s - %(name)s - %(levelname)s - %(message)s'
+        )
+        self._debug = debug
+
     async def create_match_request(self, **client_info):
         """Method docs here"""
 
-        logger.debug('[create_match_request]: %s enter', client_info['nickname'])
+        self.logger.debug('[create_match_request]: %s enter', client_info['nickname'])
 
         # here the info about the client may be saved in db
 
@@ -50,33 +48,38 @@ class MatchLogic:
                 self._match_id = 1
                 player_id = 1  # TODO: generate client id for the _match_id
 
-                logger.debug('[create_match_request]: %s unlocked event locker', client_info['nickname'])
+                self.logger.debug('[create_match_request]: %s unlocked event locker', client_info['nickname'])
                 self._event_lock.set()
 
                 await asyncio.sleep(0.00001)  # wait while other clients will be released from waiting
-                logger.debug('[create_match_request]: %s locked event locker', client_info['nickname'])
+                self.logger.debug('[create_match_request]: %s locked event locker', client_info['nickname'])
                 self._event_lock.clear()
 
+                self.logger.debug('[create_match_request]: %s  - return success response', client_info['nickname'])
                 return self._get_match_response(self._match_id, **{
                         'player_id': player_id,
                         **client_info
                     }
                 )
-        logger.debug('[create_match_request]: c=%d, eve=%s', self._waiting_count, self._event_lock.is_set())
+
+        self.logger.debug('[create_match_request]: waiting count = %d', self._waiting_count)
         if not self._event_lock.is_set():
             try:
+                self.logger.debug('[create_match_request]: %s is waiting', client_info['nickname'])
                 await asyncio.wait_for(self._event_lock.wait(), self.match_wait_time)
                 player_id = 1  # TODO: generate client id for the _match_id
 
+                self.logger.debug('[create_match_request]: %s  - return success response', client_info['nickname'])
                 return self._get_match_response(self._match_id, **{
                         'player_id': player_id,
                         **client_info
                     }
                 )
             except asyncio.TimeoutError:
-                logger.debug('[create_match_request]: %s - TimeoutError', client_info['nickname'])
+                self.logger.debug('[create_match_request]: %s - TimeoutError', client_info['nickname'])
                 await self._dec_counter()
 
+                self.logger.debug('[create_match_request]: %s  - return fail response', client_info['nickname'])
                 return self.get_error_response('timeout')
 
     async def _inc_counter(self):
@@ -112,6 +115,14 @@ class MatchLogic:
     @match_wait_time.setter
     def match_wait_time(self, new_wait_time):
         self._match_wait_time = new_wait_time
+
+    @property
+    def debug(self):
+        return self._debug
+
+    @match_wait_time.setter
+    def debug(self, new_debug_mode):
+        self._debug = new_debug_mode
 
     def get_error_response(self, text):
         """Method docs here"""
